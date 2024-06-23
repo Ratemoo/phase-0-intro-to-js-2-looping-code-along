ac4082b6a5d8a93b5e3de213bd17438f326ca0ca
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-06-24 02:01:12 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let messages = []; 
+  
+    for (let i = 0; i < namesArray.length; i++) {
+      
+      let message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message); 
+    }
+  
+    return messages; 
+  }
+  let names = ["Brian", "Bernice", "Lavynn"];
+  let event = "birthday";
+  let thankYouMessages = writeCards(names, event);
+  console.log(thankYouMessages)
+  
+
+  function countDown(){
+    let count=10
+    while(count>=0){
+      console.log(count)
+      count--
+    }
+  }
+    countDown()
+        
+    
+
+    
+  
+
